4a9a1c1eb44b6f16ca903bf74fc8ac5aa6776859
Jonas <jzhou@vwms.cn>
2016-11-23 00:26:33 +0800

    [zjq]: 修复bug，数据库端口需要转换为整型

--- database.py
+++ database.py
@@ -1,8 +1,7 @@
 import os
 from pymongo import MongoClient
 
-
 host = os.environ.get('MONGO_DB_ADDR') or '172.17.0.2'
-port = os.environ.get('MONGO_DB_PORT') or 27017
+port = int(os.environ.get('MONGO_DB_PORT')) or 27017
 
 client = MongoClient(host, port)
